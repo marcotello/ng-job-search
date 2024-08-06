b5988d2a657a0010ebb220fdb84e79fd9409deb3
Marco Tello <marcotello5@outlook.com>
2024-08-06 13:09:15 -0700

    Adding the Job Details Component and the routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,19 @@ import {fetchAllJobsResolver} from "./resolvers/fetch-all-jobs.resolver";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/job-listings', pathMatch: 'full' },
-  { path: 'job-listings', loadComponent: () => import('./jobs/jobs.component')
+  {
+    path: 'job-listings', loadComponent: () => import('./jobs/jobs.component')
       .then(c => c.JobsComponent),
     resolve: { jobs: fetchAllJobsResolver}
   },
-  { path: 'favorites', loadComponent: () => import('./favorites/favorites.component')
+  {
+    path: 'favorites', loadComponent: () => import('./favorites/favorites.component')
       .then(c => c.FavoritesComponent)
+  },
+  {
+    path: 'job-details/:jobId',
+    loadComponent: () => import('./job-details/job-details.component')
+      .then(c => c.JobDetailsComponent)
   },
   { path: '**', redirectTo: '/job-listings', pathMatch: 'full' }
 ];
